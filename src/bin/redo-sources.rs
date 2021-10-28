a29ff9227ecdd7cccc3ff0180ea0412ac0e3060b
Ross Light <ross@zombiezen.com>
2021-10-27 21:20:43 -0700

    Add copyright notices for files derived from apenwarr/redo
    I couldn't find the notice in my first pass, but discovered the
    proper attribution in cmd_redo.py.

--- src/bin/redo-sources.rs
+++ src/bin/redo-sources.rs
@@ -1,4 +1,5 @@
 // Copyright 2021 Ross Light
+// Copyright 2010-2018 Avery Pennarun and contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
